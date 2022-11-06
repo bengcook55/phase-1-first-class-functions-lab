4a6f0189e0dd57e8ee78e91cfa4b8cccaf7ba418
Benjamin Cook <bengcook55@gmail.com>
2022-11-06 17:51:24 -0500

    commit tree f36856705ae90ff4bb0403a6a2fbdf689c58877d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = () => (['Antonia', 'Nuru'])
+
+const returnLastTwoDrivers = () => (['Amari','Mo'])
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (value) => {
+    return (fare) => value * fare;
+  }
+
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driverArray, driverFunction) {
+    return driverFunction(driverArray);
+}
